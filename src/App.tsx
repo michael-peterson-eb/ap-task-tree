87cfb30248a53595bfc242b33dd51eda84a9026c
Elmer Berico <elmer.berico@infiniteblue.com>
2023-08-01 14:50:28 -0400

    updated list table

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
 
 import MultiStep from './MultiSteps';
 
-export default function App({recordInfo}) {
+export default function App({ recordInfo }) {
   return (
     <Container maxWidth="lg">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h3" gutterBottom>
-          Impact Assessment - POC
+          Impact Assessment
         </Typography>
-        <MultiStep recordInfo={recordInfo}/>
+        <MultiStep recordInfo={recordInfo} />
       </Box>
     </Container>
   );
--- src/MultiSteps.tsx
+++ src/MultiSteps.tsx
@@ -1,22 +1,29 @@
-import {useState, useEffect, Fragment, ReactNode, useRef} from 'react';
+import { useState, useEffect, Fragment, ReactNode } from 'react';
 import Stepper from '@mui/material/Stepper';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import {Box, ThemeProvider} from '@mui/material';
+import {
+  Box,
+  ThemeProvider,
+  Alert,
+  AlertTitle,
+} from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import { FormProvider, useForm } from "react-hook-form";
 import { updateQuestionWithResponse } from './model/Questions';
-import { fetchObjectSectionStatuses, updateStatusJSON } from './model/SectionStatus';
+import { updateStatusJSON } from './model/SectionStatus';
 
 import QandAForm from './QandAForm';
 import { getQuestionTypes } from './model/Assessment';
 import { NavButtonTheme } from './common/CustomTheme';
 
-export default function MultiSteps({recordInfo}) {
+export default function MultiSteps({ recordInfo }) {
   const [activeStep, setActiveStep] = useState(0);
   const [skipped, setSkipped] = useState(new Set<number>());
   const [questionTypes, setQuestionTypes] = useState([]);
-  const [formUpdated, setFormUpdated] = useState(false);
+  //const [formUpdated, setFormUpdated] = useState(false);
   const [formValues, setFormValues] = useState({});
   const [sectionStatus, setSectionStatus] = useState(recordInfo.sectionStatusesJSON);
 
@@ -28,13 +35,9 @@ export default function MultiSteps({recordInfo}) {
     FRES: "EA_SA_txtaResponse",
   };
 
-  const isStepOptional = (step: number) => {
-    return step === 1;
-  };
+  const isStepOptional = (step: number) => (step === 1);
 
-  const isStepSkipped = (step: number) => {
-    return skipped.has(step);
-  };
+  const isStepSkipped = (step: number) => (skipped.has(step));
 
   const handleNext = () => {
     let newSkipped = skipped;
@@ -44,7 +47,7 @@ export default function MultiSteps({recordInfo}) {
       newSkipped = new Set(newSkipped.values());
       newSkipped.delete(activeStep);
     }
-    if ( activeStep === questionTypes.length - 1 ) {
+    if (activeStep === questionTypes.length - 1) {
       handleReset(); // back to first step
     } else {
       setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -91,29 +94,29 @@ export default function MultiSteps({recordInfo}) {
 
   const handleSubmit = async () => {
     const formFields = Object.keys(formValues);
-    let newFormValues = {...formValues}
-    console.log("--handleSubmit--", formFields, newFormValues)
+    let newFormValues = { ...formValues }
+    //console.log("--handleSubmit--", formFields, newFormValues)
     await updateQuestionWithResponse(formValues, questionResponseFields);
     await updateStatusObject();
   }
 
   const handleChange = (type: any, event: any) => {
-    const {name, value} = event.target;
-    console.log("--handleChange--", event)
+    const { name, value } = event.target;
+    //console.log("--handleChange--", event)
     trackUpdatedQuestions(type, name, value);
   }
 
   const customChangedHandler = (type: any, _event: any, autoComplete: any) => {
-    const {name, value} = autoComplete;
-    console.log("--customChangedHandler--", value)
+    const { name, value } = autoComplete;
+    //console.log("--customChangedHandler--", value)
     trackUpdatedQuestions(type, name, value);
   }
 
   const trackUpdatedQuestions = (fieldType: any, aqId: any, value: any) => {
-    setFormUpdated(true);
+    //setFormUpdated(true);
     setFormValues({
       ...formValues,
-      [aqId]:{
+      [aqId]: {
         ...formValues[aqId],
         type: fieldType,
         value
@@ -131,89 +134,90 @@ export default function MultiSteps({recordInfo}) {
     let newValue: any = {};
     const activeType = questionTypes[activeStep]
     const typeId = activeType.id;
-    const recordId = recordInfo.id;
-    //console.log("--updateStatusObject:qTypes--", questionTypes)
-    //console.log("--updateStatusObject:before--", formValues, activeType, activeStep)
     // check if section status has been updated
     if (formValues.hasOwnProperty(typeId)) {
       const status = formValues[typeId];
       const newStatus = status.value ? "completed" : "not-started";
       newValue[`type-${typeId}`] = newStatus;
-      console.log("--updateStatusObject:NewStatus--", sectionStatus, newValue)
+      //console.log("--updateStatusObject:NewStatus--", sectionStatus, newValue)
       const newSectionStatus = { ...sectionStatus, ...newValue }
 
       // update section status state
       setSectionStatus(newSectionStatus);
-      //setQuestionTypes({...questionTypes, ...{id: typeId, name: activeType.name, status: newStatus}})
-      //console.log("--updateStatusObject--", activeType, formValues, sectionStatus, questionTypes)
-      // update statuses field
       await updateStatusJSON(recordInfo, newSectionStatus);
     }
   };
 
   return (
     <FormProvider {...formMethods}>
-       <form onSubmit={handleSubmit}>
-
-    <Box sx={{ width: '100%' }}>
-      <Stepper activeStep={activeStep}>
-      <Grid container spacing={1}>
-        {questionTypes.length > 0 && questionTypes.map((label, index) => {
-          const stepProps: { completed?: boolean } = {};
-          const labelProps: {
-            optional?: ReactNode;
-          } = {};
-          if (isStepOptional(index)) {
-            labelProps.optional = (
-              <Typography variant="caption">Optional</Typography>
-            );
-          }
-          if (isStepSkipped(index)) {
-            stepProps.completed = false;
-          }
-          return (
-            <Grid item xs={(12 / questionTypes.length)}>
-              <ThemeProvider theme={NavButtonTheme}>
-                <Button color={activeStep == index ? 'active' : 'neutral'} variant="contained" style={{textTransform: 'none', color: activeStep == index ? '#FFF' : '#000'}} fullWidth onClick={() => setActiveStep(index)}>
-                  {label.name}
-                </Button>
-              </ThemeProvider>
+      <form onSubmit={handleSubmit}>
+        <Box sx={{ width: '100%' }}>
+          <Stepper activeStep={activeStep}>
+            <Grid container spacing={1}>
+              {questionTypes.length > 0 && questionTypes.map((label, index) => {
+                const stepProps: { completed?: boolean } = {};
+                const labelProps: {
+                  optional?: ReactNode;
+                } = {};
+                if (isStepOptional(index)) {
+                  labelProps.optional = (
+                    <Typography variant="caption">Optional</Typography>
+                  );
+                }
+                if (isStepSkipped(index)) {
+                  stepProps.completed = false;
+                }
+                return (
+                  <Grid item xs={(12 / questionTypes.length)}>
+                    <ThemeProvider theme={NavButtonTheme}>
+                      <Button
+                        color={activeStep == index ? 'active' : 'neutral'}
+                        variant="contained"
+                        style={{ textTransform: 'none', color: activeStep == index ? '#FFF' : '#000' }}
+                        fullWidth
+                        onClick={() => setActiveStep(index)}
+                      >
+                        {label.name}
+                      </Button>
+                    </ThemeProvider>
+                  </Grid>
+                );
+              })}
             </Grid>
-          );
-        })}
-        </Grid>
-      </Stepper>
-      {activeStep === questionTypes.length ? (
-        <Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
-          </Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Box sx={{ flex: '1 1 auto' }} />
-            <Button onClick={handleReset}>Reset</Button>
-          </Box>
-        </Fragment>
-      ) : (
-        <Fragment>
-          <QandAForm recordInfo={recordInfo} qtype={questionTypes[activeStep]} handleFormValues={setFormValues} handleOnChange={handleChange} customChangedHandler={customChangedHandler}/>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 1 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-              Back
-            </Button>
-            <Button onClick={handleNext}>
-              {activeStep === questionTypes.length - 1 ? 'Finish' : 'Next'}
-            </Button>
-          </Box>
-        </Fragment>
-      )}
-    </Box>
-
-    </form>
+          </Stepper>
+          {activeStep === questionTypes.length ? (
+            <Fragment>
+              <Typography sx={{ mt: 2, mb: 1 }}>
+                <Alert sx={{ marginTop: '12px' }} severity="warning">
+                  <AlertTitle>No assessment questions record found!</AlertTitle>
+                </Alert>
+              </Typography>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <QandAForm
+                recordInfo={recordInfo}
+                qtype={questionTypes[activeStep]}
+                handleFormValues={setFormValues}
+                handleOnChange={handleChange}
+                customChangedHandler={customChangedHandler} />
+              <Box sx={{ display: 'flex', flexDirection: 'row', pt: 1 }}>
+                <Button
+                  color="inherit"
+                  disabled={activeStep === 0}
+                  onClick={handleBack}
+                  sx={{ mr: 1, backgroundColor: '#DDD', fontSize: '1.2rem' }}
+                >
+                  <FontAwesomeIcon icon={faChevronLeft} />
+                </Button>
+                <Button onClick={handleNext} sx={{ backgroundColor: '#DDD', fontSize: '1.2rem' }}>
+                  <FontAwesomeIcon icon={faChevronRight} />
+                </Button>
+              </Box>
+            </Fragment>
+          )}
+        </Box>
+      </form>
     </FormProvider>
   );
 }
