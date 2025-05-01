babca79c0ef0e9ddab5eef78c3084168ebc7abb6
Michael Peterson <michael.peterson@infiniteblue.com>
2025-05-01 16:22:57 -0400

    update for view mode

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,19 @@ export const queryClient = new QueryClient();
 export default function App({ recordInfo: appParams }) {
   if (!appParams) return <div>Please provide params in BCIC Design Page to render the component</div>;
 
+  setTimeout(() => {
+    const viewModeParentContainer = window.document.getElementsByClassName("k-content k-state-active")[0];
+
+    /** Remove overflow from view mode parent container, this is necessary for the carousel to be sticky. The container style
+     * we want to modify is not in the react application itself. It is in a parent container, so we need to access it via the DOM.
+     * The timeout is necessary to allow the view mode container to be rendered before we can access it. This is an ugly but
+     * necessary workaround.
+     * */
+
+    //@ts-expect-error
+    viewModeParentContainer.style.overflow = "visible";
+  }, 0);
+
   return (
     <Container maxWidth={false} disableGutters>
       <Box sx={{ m: 1 }}>
--- src/components/OpSecHeader.tsx
+++ src/components/OpSecHeader.tsx
@@ -2,7 +2,7 @@ import { useMediaQuery } from "@mui/material";
 import Carousel from "./Carousel/Carousel";
 import { CarouselDropdown } from "./Carousel/CarouselDropdown";
 
-export const OpSecHeader = ({ operationSections, selectedOpsSec, setSelectedOpsSection }) => {
+export const OpSecHeader = ({ operationSections, selectedOpsSec, setSelectedOpsSection, mode }) => {
   const smallScreen: boolean = useMediaQuery("(max-width:640px)");
 
   const handleTabClick = (index: number) => {
@@ -10,7 +10,7 @@ export const OpSecHeader = ({ operationSections, selectedOpsSec, setSelectedOpsS
   };
 
   return !smallScreen ? (
-    <Carousel operationSections={operationSections} selectedOpsSec={selectedOpsSec} handleTabClick={handleTabClick} />
+    <Carousel operationSections={operationSections} selectedOpsSec={selectedOpsSec} handleTabClick={handleTabClick} mode={mode} />
   ) : (
     <CarouselDropdown operationSections={operationSections} selectedOpsSec={selectedOpsSec} handleTabClick={handleTabClick} />
   );
