3fab722fd91ca5c322d610e4bf94b46ba028ca4e
Michael Peterson <michael.peterson@infiniteblue.com>
2025-09-12 13:17:59 -0400

    importing from shared repo to be fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import "bcic-shared-ui/styles.css";
+import { Button } from "bcic-shared-ui";
+
 export default function App() {
-  return <div>App container</div>;
+  const handleClick = () => {
+    alert("Button clicked!");
+  };
+
+  return (
+    <div>
+      <div>App container</div>
+      <Button onClick={handleClick}>Test</Button>
+      {/* <Select options={severityChips} /> */}
+    </div>
+  );
 }
